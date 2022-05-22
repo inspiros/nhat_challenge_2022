f915734237c661edfa120c27a68d67485b0e7958
inspiros <inspiros.tran@gmail.com>
2022-05-22 21:08:29 +0700

    test new gcn architecture

--- models/simple_gcn.py
+++ models/simple_gcn.py
@@ -93,29 +93,27 @@ class SimpleGCN(nn.Module):
 
         self.data_bn = nn.BatchNorm1d(self.in_channels * self.graph.num_node_each, 0.1)
 
-        self.gcn1 = GCNBlock(self.in_channels, 64, kernel_size, dropout=dropout)
+        self.gcn0 = GCNBlock(self.in_channels, 32, kernel_size, dropout=dropout)
+        self.gcn1 = GCNBlock(64, 64, kernel_size, dropout=dropout)
         self.pool1 = GraphMaxPool(self.graph)
 
         self.gcn2 = GCNBlock(64, 128, kernel_size_pool, dropout=dropout)
         self.pool2 = GraphMaxPool(self.graph_pool)
 
-        self.conv0 = nn.Sequential(
-            nn.Conv2d(128, 128, kernel_size=(1, 1)),
+        self.conv = nn.Sequential(
+            nn.Conv2d(128, 128, kernel_size=(1, 1), padding=(0, 0)),
             # nn.BatchNorm2d(128, momentum=0.1),
             nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
+            nn.Dropout(dropout),
         )
 
         self.upsample1 = GraphUpsample(self.graph_pool)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=(1, 1)),
-            nn.BatchNorm2d(64, momentum=0.1),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout)
-        )
+        self.gcn3 = GCNBlock(128, 64, kernel_size_pool, dropout=dropout)
 
         self.upsample2 = GraphUpsample(self.graph)
-        self.conv2 = nn.Conv2d(64, self.out_channels, (1, 1))
+        self.gcn4 = GCNBlock(64, 32, kernel_size, dropout=dropout)
+
+        self.fcn = nn.Conv2d(32, self.out_channels, kernel_size=(1, 1))
 
     def forward(self, x):
         N, C, T, V = x.size()
@@ -125,18 +123,20 @@ class SimpleGCN(nn.Module):
         x = self.data_bn(x)
         x = x.view(N, V, C, T).permute(0, 2, 3, 1).contiguous().view(N, C, T, V)
 
+        x, _ = self.gcn0(x, self.A)
         x, _ = self.gcn1(x, self.A)
         x = self.pool1(x)
 
         x, _ = self.gcn2(x, self.A_pool)
         x = self.pool2(x)
 
-        x = self.conv0(x)
+        x = self.conv(x)
 
         x = self.upsample1(x)
-        x = self.conv1(x)
+        x, _ = self.gcn3(x, self.A_pool)
 
         x = self.upsample2(x)
-        x = self.conv2(x)
+        x, _ = self.gcn4(x, self.A)
+        x = self.fcn(x)
 
         return x
